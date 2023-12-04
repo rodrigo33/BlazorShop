5dfd7f338fbb630afe8649ce540521702951fec0
rodrigo33 <rbarbosa.barbosa@gmail.com>
2023-12-04 16:41:45 -0300

    Criação das entities, classe de contexto

---
+++ BlazorShop.Api/Entities/Carrinho.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.ObjectModel;
+
+namespace BlazorShop.Api.Entities;
+
+public class Carrinho
+{
+    public int Id { get; set; }
+    public int UsuarioId { get; set; }
+    public Collection<CarrinhoItem> Itens { get; set; }
+        = new Collection<CarrinhoItem>();
+}
+
---
+++ BlazorShop.Api/Entities/Usuario.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorShop.Api.Entities;
+
+public class Usuario
+{
+    public int Id { get; set; }
+    public string NomeUsuario { get; set; } = string.Empty;
+
+    public Carrinho? Carrinho { get; set; }
+}
